3003b784915e22e620c435d857a33fd580a523aa
Emiliayi <emilia.yi.hao@googlemail.com>
2014-03-25 14:31:58 +0100

    fixed #6 checkWeights function added

---
+++ chkFuncs/checkWeights.m
@@ -0,0 +1,21 @@
+function checkWeights(weightsMatrix)
+
+% tolerance level
+tolEps = 1e-6;
+
+% column vector / row vector?
+if size(weightsMatrix, 2) == 1;
+    % if column vector, transpose
+    weightsMatrix = weightsMatrix';
+end
+
+% no column vector may exist anymore
+
+for ii=1:size(weightsMatrix, 1)   
+    if abs(sum(weightsMatrix(ii, :)) - 1) > tolEps;
+        error('portfolio weights do not sum to one!')
+    end
+end
+
+end
+
