46bca70a33727e5120903bbf484a62f22bff6738
mffuser <Christian.Groll@stat.uni-muenchen.de>
2014-03-25 17:50:37 +0100

    added documentation to table split and combine

--- splitTable.m
+++ splitTable.m
@@ -1,8 +1,16 @@
-function [vals,stockNames,dates] = splitTable( inputTable )
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
-vals = table2array(inputTable);
-stockNames = inputTable.Properties.VariableNames;
-dates = inputTable.Properties.RowNames;
+function [vals, stockNames, dates] = splitTable(tableData)
+%SPLITTABLE Split table into individual components
+%
+% Inputs:
+%   tableData   table containing stock returns with data and names
+%
+% Outputs:
+%   vals        mxn matrix of return data
+%   stockNames  1xn cell array of stock names
+%   dates       mx1 cell array of dates as strings
+
+vals = table2array(tableData);
+stockNames = tableData.Properties.VariableNames;
+dates = tableData.Properties.RowNames;
 end
 
