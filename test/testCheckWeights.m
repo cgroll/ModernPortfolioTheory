830af504dab22fd6f253159aa12798a358a2217a
Emiliayi <emilia.yi.hao@googlemail.com>
2014-03-26 12:37:30 +0100

    function checkWeights: get rid of for loop and updated unit test

--- test/testCheckWeights.m
+++ test/testCheckWeights.m
@@ -19,7 +19,27 @@ checkWeights(weightMatrixT);
 % actResult = checkWeights(weightMatrixT);
 % verifyEqual(testCase, actResult, true);
 end
-% 
+%
+function testValidInputTable(testCase)
+weightMatrixF = rand(200,10);
+sumCol = sum(weightMatrixF, 2);
+sumMatrix = sumCol*ones(1,10);
+weightMatrixT = weightMatrixF./sumMatrix;
+weightTableT = array2table(weightMatrixT);
+
+checkWeights(weightTableT);
+% actResult = checkWeights(weightMatrixT);
+% verifyEqual(testCase, actResult, true);
+end
+
+function testValidInputVector(testCase)
+weightVectorF = rand(10, 1);
+weightVectorT = weightVectorF/sum(weightVectorF);
+
+checkWeights(weightVectorT);
+% actResult = checkWeights(weightMatrixT);
+% verifyEqual(testCase, actResult, true);
+end
 
 function testInvalidInput(testCase)
 % import matlab.unittest.TestCase;
@@ -30,6 +50,33 @@ weightMatrixF = rand(200,10);
 %     Throws('SOME:error:id'));
 
 verifyError(testCase, @() checkWeights(weightMatrixF), 'portf:weights');
+% actResult = checkWeights(weightMatrixF);
+% verifyEqual(testCase, actResult, false);
+end
+
+function testInvalidInputTable(testCase)
+% import matlab.unittest.TestCase;
+% import matlab.unittest.constraints.Throws;
+
+weightMatrixF = rand(200,10);
+% testCase.verifyThat(@() error('SOME:error:id','Error!'), ...
+%     Throws('SOME:error:id'));
+weightTableF = array2table(weightMatrixF);
+
+verifyError(testCase, @() checkWeights(weightTableF), 'portf:weights');
+% actResult = checkWeights(weightMatrixF);
+% verifyEqual(testCase, actResult, false);
+end
+
+function testInvalidInputVector(testCase)
+% import matlab.unittest.TestCase;
+% import matlab.unittest.constraints.Throws;
+
+weightVectorF = rand(10, 1);
+% testCase.verifyThat(@() error('SOME:error:id','Error!'), ...
+%     Throws('SOME:error:id'));
+
+verifyError(testCase, @() checkWeights(weightVectorF), 'portf:weights');
 % actResult = checkWeights(weightMatrixF);
 % verifyEqual(testCase, actResult, false);
 end
