68fa69d37354c1c4fc7cb3107ca39fc4542ee221
thomasMff <thomasMff2@gmail.com>
2014-03-31 12:59:17 +0200

    analyse continued

--- analysePortfolioReturns.m
+++ analysePortfolioReturns.m
@@ -1,11 +1,11 @@
 function[structValues] = analysePortfolioReturns(returns, varP)
-% calculates mean, volatility and Value at Risk
+% calculates mean, volatility and empirical value at risk
 % input: returns as vector, varP as double in the interval [0,1]
 % output: struct with mean, volatility and VaR
 
 returns=returns{:,:};
 meanReturn = mean(returns);
-volatility = sd(returns);
+volatility = std(returns);
 VaR = quantile(returns, 1-varP);
 %sharpe = 
 structValues=struct('mean', meanReturn, 'volatility', volatility, 'VaR', VaR);
