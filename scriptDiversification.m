683678b8e47b24e3defc1f0241d130ad5e647680
thomasMff <thomasMff2@gmail.com>
2014-04-02 11:27:25 +0200

    diversificationM.m measures for matrices and tables. making diversification.m obsolete. not yet tested for tables.

---
+++ scriptDiversification.m
@@ -0,0 +1,7 @@
+divMTest=diversificationM([0 0 1 2; 1 -1 5 4; -3 4 16 0.000000001])
+divM.Test=numberAssets
+divMTest.numberSignificantAssets
+divMTest.maxWeight
+divMTest.stdWeights
+divMTest.numberShort
+divMTest.gini
