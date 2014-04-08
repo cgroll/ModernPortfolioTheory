77a573183884bd7ad2ae06f91039843aa2c3c0d6
thomasMff <thomasMff2@gmail.com>
2014-04-08 17:47:56 +0200

    plot function for maximum drawdown

---
+++ performance/plotMaxDrawdown.m
@@ -0,0 +1,36 @@
+function [] = plotMaxDrawdown(maxDrawdownStruct, discRetVector)
+% plots prices calculated from return series (table vector) and
+% maximum drawdown period
+
+% input: discrete return series as table (only 1 column), struct returned 
+%        from maxDrawdown function
+% output: none
+
+prices=cumprod(discRetVector{:,:}+1);
+
+% plot price series
+plot(prices);
+
+% attempt to have x axis as date
+% plot(str2num(discRetVector.Properties.RowNames{:,:}), prices);
+% datetick 'x';
+
+
+xlabel ('time');
+ylabel 'price';
+title 'maximum drawdown';
+hold on;
+
+% plot maximum drawdown
+plot(maxDrawdownStruct.Index(1):maxDrawdownStruct.Index(2),...
+    prices(maxDrawdownStruct.Index(1):maxDrawdownStruct.Index(2)),...
+    'color','red');
+
+%attempt to have x-axis as date
+% plot(discRetVector.Properties.RowNames(maxDrawdownStruct.Index(1):maxDrawdownStruct.Index(2)),...
+%     prices(maxDrawdownStruct.Index(1):maxDrawdownStruct.Index(2)),...
+%     'color','red');
+shg
+
+
+end
--- scriptMaximumDrawdown.m
+++ scriptMaximumDrawdown.m
@@ -1,6 +1,12 @@
+loadData;
+
+%%
 maxDraw = maxDrawdown(discRet(:,1));
-prices=cumprod(discRet{:,1}+1);
-plot(prices);
-hold on;
-plot(maxDraw.Index(1):maxDraw.Index(2) ,prices(maxDraw.Index(1):maxDraw.Index(2)), 'color','red');
+
+%prices=cumprod(discRet{:,1}+1);
+%plot(prices);
+%hold on;
+%plot(maxDraw.Index(1):maxDraw.Index(2) ,prices(maxDraw.Index(1):maxDraw.Index(2)), 'color','red');
 % algorithm works!
+
+plotMaxDrawdown(maxDraw, discRet(:,1));
