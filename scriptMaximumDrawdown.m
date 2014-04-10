6244522bf9ce1dc2148beacb80071486b6d92b85
thomasMff <thomasMff2@gmail.com>
2014-04-10 15:25:23 +0200

    script forgotten max drawdown

--- scriptMaximumDrawdown.m
+++ scriptMaximumDrawdown.m
@@ -2,6 +2,8 @@ loadData;
 
 %%
 maxDraw = maxDrawdown(discRet(:,1));
+%  maxDrawdownStandardized: -0.5437 can be interpreted as the price dropped 
+% at most  about 54% 
 
 %prices=cumprod(discRet{:,1}+1);
 %plot(prices);
@@ -10,3 +12,6 @@ maxDraw = maxDrawdown(discRet(:,1));
 % algorithm works!
 
 plotMaxDrawdown(maxDraw, discRet(:,1));
+
+%% wikipedia algorithm
+maxDrawWiki = maxDrawdownWiki(discRet(:,1));
