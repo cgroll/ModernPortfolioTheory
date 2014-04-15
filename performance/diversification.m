f23172f8c39207b2b3c50a97779a29fd468c4f06
Christian Groll <groll.christian.edu@gmail.com>
2014-04-15 11:59:12 +0200

    unique diversification function implemented fixes #28

--- performance/diversification.m
+++ performance/diversification.m
@@ -1,32 +1,62 @@
-function [structValues] = diversification(weights)
+function [divIndicators] = diversification(weights)
 % calculates several diversification measures
 %
-% input: weights vector
-% output: number of assets, number of significant assets, maximum weight,
-%         standard deviation of weights, number of negative weights and
-%         gini coefficient (herfindahl)
+% input:    
+%   weights     weights vector, matrix or table - weights in rows
+%
+% output: 
+%   divIndicators   table containing information on number of assets, 
+%                   number of significant assets, maximum weight,
+%                   standard deviation of weights, number of negative 
+%                   weights and gini coefficient (herfindahl) for each day
 
+% specify tolerance level for significant weights
+sigLev = 0.0001;
 
-if istable(weights)
-    weights = weights{:,:};
+checkWeights(weights)
+
+% transpose if column vector
+if ~istable(weights)
+    if size(weights, 2) == 1
+        % if column vector, transpose
+        weights = weights';
+    end
 end
 
-numberAssets = sum(weights~=0);
-numberSignificantAssets = sum(abs(weights-0)>0.0001);
-maxWeight = max(weights);
-stdWeights = std(weights);
-numberShort = sum(weights<0);
+% extract rownames from table
+if istable(weights)
+    rownames = weights.Properties.RowNames;
+    weights = weights{:,:};
+else
+    rownames = {};
+end
+
+
+% get diversification indicators for each row
+numberAssets = sum(weights~=0,2);
+numberSignificantAssets = sum(abs(weights-0) > sigLev,2);
+maxWeight = max(weights,[],2);
+stdWeights = std(weights,[],2);
+numberShort = sum(weights<0,2);
 
 % gini coefficient for negative weights
 absweights = abs(weights);
-absweights = absweights./repmat(sum(absweights,2),1,size(weights,2));
-gini = sum(absweights.^2);
+sums = sum(absweights,2);
+repsums = repmat(sums,1,size(weights,2));
+absweights = absweights./repsums;
+gini = sum(absweights.^2,2);
 % Herfindahl index as special case of gini coefficient because of the 
 % limited number of assets 
 
+divIndicators = table(numberAssets, ...
+    numberSignificantAssets, ...
+    maxWeight, ...
+    stdWeights, ...
+    numberShort, ...
+    gini, ...
+    'VariableNames', ...
+    {'nAss', 'nSignificantAss', 'maxWeight', 'std', 'nShort', 'gini'});
 
+divIndicators.Properties.RowNames = rownames;
 
-structValues = struct('numberAssets', numberAssets, 'numberSignificantAssets'...
-    ,numberSignificantAssets, 'maxWeight', maxWeight, 'stdWeights',stdWeights,...
-    'numberShort', numberShort, 'gini',gini);
 end
